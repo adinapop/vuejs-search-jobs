6dbffd215b31d315c334ed34e3d4f98cdb224e75
adinapop <adinampop@yahoo.com>
2021-07-06 11:32:42 +0200

    Repo created and M1 done

---
+++ js/script.js
@@ -0,0 +1,82 @@
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: "#app",
+        data: {
+            jobs: [
+                {
+                    id: 1,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 2,
+                    company: 'Perferendis',
+                    position: 'Senior Web Developer',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 3,
+                    company: 'Perferendis',
+                    position: 'Junior Web Developer',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 4,
+                    company: 'Perferendis',
+                    position: 'Graphic Design',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 5,
+                    company: 'Perferendis',
+                    position: 'Software engineers',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 6,
+                    company: 'Perferendis',
+                    position: 'Web Designer',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+            ],
+            starred: [1, 3, 4, 6],
+            applied: [2, 5]            
+        },
+
+        methods: {
+            
+            // function to get company logo
+            getLogo: function(job) {
+                return "./img/" + job.logo + ".jpg";
+            },
+
+        }
+    }
+)
